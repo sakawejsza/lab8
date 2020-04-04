4f4761fdab73c85424d649fe38a0853305bb0367
Shelby Kawejsza <sak123@tamu.edu>
2020-04-03 23:40:49 -0500

    adds main.py file

---
+++ python/main.py
@@ -0,0 +1,20 @@
+##### SWAP
+#Shelby Kawejsza -UIN 326001573 TCMG272-Section 500
+
+##### Imports
+
+##### Function Definitions
+
+##### Main Program
+a = (input('Give me an interger number ->A:'))#ask for a
+z = (input('Give me an interger number ->Z:'))#ask for z
+
+
+#swap them
+temp = a
+a = z
+z = temp
+print('A is now:', a)
+print('Z is now:', z)
+
+
